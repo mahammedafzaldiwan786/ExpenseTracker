643bab0051aed8724a99d3c1063ce80e6e9b9ab4
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2025-02-14 16:45:00 +0530

    City JSP,Controller,Entity,Repository add

---
+++ src/main/java/com/grownited/controller/CityController.java
@@ -0,0 +1,36 @@
+package com.grownited.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.grownited.entity.CityEntity;
+import com.grownited.repository.CityRepository;
+
+
+@Controller
+public class CityController {
+	
+	@Autowired
+	CityRepository repositoryCity;
+	
+	
+	
+	
+	@GetMapping("newcity")
+	public String newcity() {
+		
+		return "NewCity";
+	}
+	
+	@PostMapping("savecity")
+	public String savecity(CityEntity entityCity) {
+		
+		repositoryCity.save(entityCity);
+		
+		return "NewCity";
+	}
+	
+	
+}
